1888524570b179d976fb4563f3f9cbfa64e15220
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2025-12-06 00:16:24 +0600

    Set all config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,8 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-  port: process.env.PORT || 3000,
-  nodeEnv: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 5000,
+  nodeEnv: "development",
+  databaseUrl: process.env.CONNECTION_STR,
+  jwtSecret: process.env.JWT_SECRET,
 };
 
 export default config;
